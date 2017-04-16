5f0f171fc888936705f6998e28f87501f78f8315
Jakub Martyčák <martycakjakub@gmail.com>
2017-04-17 00:02:56 +0200

    Add bundle js script to travis

---
+++ scripts/bundle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if $TRAVIS_BUILD_NUMBER
+then
+  BUILD_NUMBER="$TRAVIS_BUILD_NUMBER"
+fi
+  BUILD_NUMBER="100000"
+
+echo "Creating jsbundle for platform $1 build number $BUILD_NUMBER"
+mkdir -p ".bundles/$1/$BUILD_NUMBER"
+react-native bundle --entry-file "index.$1.js" --bundle-output ".bundles/$1/$BUILD_NUMBER/main.jsbundle" --dev "false" --platform "$1" --assets-dest ".bundles/$1/$BUILD_NUMBER" --sourcemap-output ".bundles/$1/$BUILD_NUMBER/main.js.map"
