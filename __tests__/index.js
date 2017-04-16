0ad27f0c03b44a67c106df6c578d05ea17357cad
Jakub Martyčák <martycakjakub@gmail.com>
2017-04-16 21:54:18 +0200

    Make npm test pass

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,12 +1,13 @@
 import 'react-native'
 import React from 'react'
-import Index from '../index.ios.js'
+import Index from '../app/scenes/About/index.js'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
+it('renders correctly', (done) => {
   const tree = renderer.create( // eslint-disable-line
     <Index />
   )
+  done()
 })
