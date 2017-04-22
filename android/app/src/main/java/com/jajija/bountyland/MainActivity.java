ecfc9308b0b1fcd38e95435c3a1a6dfb4faa06e1
Jakub Martyčák <martycakjakub@gmail.com>
2017-04-22 11:05:24 +0200

    [android] Add app icon and splash screen

--- android/app/src/main/java/com/jajija/bountyland/MainActivity.java
+++ android/app/src/main/java/com/jajija/bountyland/MainActivity.java
@@ -3,5 +3,9 @@ package com.jajija.bountyland;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+    @Override
+    public int getSplashLayout() {
+        return R.layout.splash_screen;
+    }
 
 }
