f95346cff403b20d3252daceb8f1b070b1a5c7f4
Jakub Martyčák <martycakjakub@gmail.com>
2017-04-22 23:57:22 +0200

    [android] Improve android project

--- android/app/src/main/java/com/jajija/bountyland/MainApplication.java
+++ android/app/src/main/java/com/jajija/bountyland/MainApplication.java
@@ -2,7 +2,6 @@ package com.jajija.bountyland;
 
 import android.content.Context;
 import android.content.Intent;
-// import android.support.multidex.MultiDex;
 
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 
@@ -34,12 +33,6 @@ public class MainApplication extends NavigationApplication {
     }
 
     @Override
-    protected void attachBaseContext(Context context) {
-        super.attachBaseContext(context);
-       //  MultiDex.install(this);
-    }
-
-    @Override
     public void onCreate() {
         super.onCreate();
         FacebookSdk.sdkInitialize(getApplicationContext());
@@ -52,7 +45,6 @@ public class MainApplication extends NavigationApplication {
             }
         });
     }
-
 
     @Override
     public boolean isDebug() {
